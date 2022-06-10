b41a48235560059117fac9184ad458c538b887b6
Abel-desu <95746961+Abel-desu@users.noreply.github.com>
2022-06-10 21:04:56 +0300

    initialize project

--- src/index.js
+++ src/index.js
@@ -1,34 +1,152 @@
 import './style.css';
-// Create object with different hardcoded elements
-const todolists = document.querySelector('.todo-container');
 
-const todolist = [
-  {
-    index: 1,
-    description: 'Wash the dishes',
-    completed: false,
-  },
+const textInput = document.querySelector('input');
+const todosMaincontainer = document.querySelector('.todos-container');
+const clearAllbtn = document.querySelector('button');
 
-  {
-    index: 1,
-    description: 'Complete todo list project',
-    completed: false,
-  },
-];
+class MyObject {
+  constructor(description, completed, index) {
+    this.description = description;
+    this.completed = completed;
+    this.index = index;
+  }
+}
+const updateLocal = () => {
+  const localData = JSON.parse(localStorage.getItem('list'));
+  const todos = document.querySelectorAll('span');
+  for (let i = 0; i < todos.length; i += 1) {
+    if (todos[i].classList.contains('checkTodo')) {
+      localData[i].completed = true;
+    } else {
+      localData[i].completed = false;
+    }
+  }
+  localStorage.setItem('list', JSON.stringify(localData));
+};
+const myArray = [];
+const addTodo = (todoValue) => {
+  const todoContainer = document.createElement('div');
+  todoContainer.className = 'todoContainer';
+  todoContainer.innerHTML += `
+      <input type="checkbox" class="checkbox">
+      <span>${todoValue}</span>
+      <i class="fas fa-ellipsis-v"></i>
+      <i class="fas fa-trash-alt"></i>
+    `;
+  todosMaincontainer.appendChild(todoContainer);
 
-const displayLists = () => {
-  todolist.forEach((list) => {
-    const todoContainer = document.createElement('div');
-    todoContainer.className = 'list border-bottom';
-    todoContainer.innerHTML += `
-          <div>
-          <input type="checkbox" class="check">
-          <span>${list.description}</span>
-          </div>
-          <i class="fa-solid fa-ellipsis-vertical"></i>
-          `;
-    todolists.append(todoContainer);
+  const checkbox = document.querySelectorAll('.checkbox');
+  checkbox.forEach((i) => {
+    i.addEventListener('click', () => {
+      i.parentElement.classList.toggle('checkedContainer');
+      i.nextElementSibling.classList.toggle('checkTodo');
+      i.parentElement.lastElementChild.classList.toggle('trash-active');
+      i.parentElement.lastElementChild.previousElementSibling.classList.toggle('edit-desable');
+      updateLocal();
+    });
+  });
+  /* eslint-disable */
+  const editTodo = (todoContainer, todo) => {
+    const editInput = document.createElement('input');
+    editInput.type = 'text';
+    editInput.className = 'editInput';
+    editInput.value = todo.textContent;
+    todoContainer.replaceChild(editInput, todo);
+    editInput.addEventListener('keypress', (e) => {
+      if (e.key === 'Enter') {
+        const todoContainers = document.querySelectorAll('.todoContainer');
+        const localData = JSON.parse(localStorage.getItem('list'));
+        for (let i = 0; i < todoContainers.length; i += 1) {
+          if (todoContainers[i].classList.contains('checkedContainer')) {
+            localData[i].description = editInput.value;
+            localStorage.setItem('list', JSON.stringify(localData));
+          }
+        }
+  
+        editInput.parentElement.classList.remove('checkedContainer');
+        todoContainer.replaceChild(todo, editInput);
+        todo.textContent = editInput.value;
+      }
+    });
+  };
+  const object = new MyObject(todoValue, false, checkbox.length - 1);
+  myArray.push(object);
+  localStorage.setItem('list', JSON.stringify(myArray));
+
+  const editIcons = document.querySelectorAll('.fa-ellipsis-v');
+  editIcons.forEach((i) => {
+    i.addEventListener('click', () => {
+      i.parentElement.classList.add('checkedContainer');
+      editTodo(todoContainer, i.previousElementSibling);
+    });
+  });
+
+  const removeIcons = document.querySelectorAll('.fa-trash-alt');
+  removeIcons.forEach((i) => {
+    i.addEventListener('click', () => {
+      removeTodo(i.parentElement);
+    });
   });
 };
+ /* eslint-disable */
+const removeTodo = (todo) => {
+  todosMaincontainer.removeChild(todo);
+  let count = 0;
+  const localData = JSON.parse(localStorage.getItem('list'));
+  const data = Array.from(localData).filter((i) => i.completed === false);
+  data.map((i) => i.index = count += 1);
+  localStorage.setItem('list', JSON.stringify(data));
+};
 
-displayLists();
+
+textInput.addEventListener('keypress', (e) => {
+  if (e.key === 'Enter' && textInput.value) {
+    e.preventDefault();
+    addTodo(textInput.value);
+    textInput.value = null;
+  }
+});
+
+const getFromLocal = () => {
+  const data = JSON.parse(localStorage.getItem('list'));
+  data.map((i) => {
+    myArray.push(i);
+    const todoContainer = document.createElement('div');
+    todoContainer.className = 'todoContainer';
+    todoContainer.innerHTML += `
+          <input type="checkbox" class="checkbox">
+          <span>${i.description}</span>
+          <i class="fas fa-ellipsis-v"></i>
+          <i class="fas fa-trash-alt"></i>
+        `;
+    todosMaincontainer.appendChild(todoContainer);
+
+    const editIcons = document.querySelectorAll('.fa-ellipsis-v');
+    editIcons.forEach((i) => {
+      i.addEventListener('click', () => {
+        editTodo(todoContainer, i.previousElementSibling);
+        i.parentElement.classList.add('checkedContainer');
+      });
+    });
+  });
+  const checkbox = document.querySelectorAll('.checkbox');
+  checkbox.forEach((i) => {
+    i.addEventListener('click', () => {
+      i.parentElement.classList.toggle('checkedContainer');
+      i.nextElementSibling.classList.toggle('checkTodo');
+      i.parentElement.lastElementChild.classList.toggle('trash-active');
+      i.parentElement.lastElementChild.previousElementSibling.classList.toggle('edit-desable');
+      updateLocal();
+    });
+  });
+  const removeIcons = document.querySelectorAll('fa-trash-alt');
+  removeIcons.forEach((i) => {
+    i.addEventListener('click', () => {
+      removeTodo(i.parentElement);
+    });
+  });
+  localStorage.setItem('list', JSON.stringify(myArray));
+};
+window.addEventListener('load', getFromLocal);
+
+
